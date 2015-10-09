04ee9d97c9c1eebbe77646ba253568ffaf095e47
Jon Khaykin <jon@jonkhaykin.com>
2015-10-09 14:18:57 -0700

    Config variables fixed

--- fb_dailycal.py
+++ fb_dailycal.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 manager = Manager(app)
 
-r = requests.get('https://graph.facebook.com/266259930135554/feed?access_token=' + ACCESS_TOKEN + '&fields=message,likes.limit(500),created_time')
+r = requests.get('https://graph.facebook.com/266259930135554/feed?access_token=' + os.environ['ACCESS_TOKEN'] + '&fields=message,likes.limit(500),created_time')
 json_data = r.json()['data']
 word_list = "arson assault blackmail burglary fraud hijacking kidnapping mugging mugger murderer robber shoplifter smuggler terrorist thief vandal appeal barrister caution cell community service court court case death penalty defense fine jail guilty imprisonment innocent judge jury justice lawyer offence sentence prison probation prosecution punishment corporal punishment remand home solicitor trial verdict witness arrest ban break burgle charge escape investigate rob steal armed burglar alarm illegal weapon gun knife incident accident homeless"
 
@@ -20,12 +20,12 @@ def index():
         fb_text = json_data[i]['message']
         if datetime.now().utcnow() <= p_time + timedelta(hours = 1):
             if any(word in fb_text for word in word_list.split()):
-                mandrill_client = mandrill.Mandrill(MANDRILL_KEY)
+                mandrill_client = mandrill.Mandrill(os.environ['MANDRILL_KEY'])
                 message = {'text': fb_text,
-                    'from_email': FROM_EMAIL,
+                    'from_email': os.environ['FROM_EMAIL'],
                     'from_name': 'Daily Cal Alert',
                     'subject': 'Daily Cal Alert',
-                    'to': [{'email': TO_EMAIL,
+                    'to': [{'email': os.environ['TO_EMAIL'],
                          'name': 'Daily Cal',
                          'type': 'to'}]}
                 result = mandrill_client.messages.send(message=message)
