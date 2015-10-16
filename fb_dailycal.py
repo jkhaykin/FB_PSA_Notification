b438098fbb9ccb3f03f122908416ff0a21d8c12d
Jon Khaykin <jon@jonkhaykin.com>
2015-10-16 12:05:04 -0700

    Added documentation

--- fb_dailycal.py
+++ fb_dailycal.py
@@ -8,19 +8,29 @@ import os
 app = Flask(__name__)
 manager = Manager(app)
 
+'''Send request to graph API to get the messages, likes counts, and created times
+for the UC Berkeley Free and For Sale page (FB ID: 266259930135554)'''
 r = requests.get('https://graph.facebook.com/266259930135554/feed?access_token=' + os.environ['ACCESS_TOKEN'] + '&fields=message,likes.limit(500),created_time')
 json_data = r.json()['data']
+
+# list of words to check for in Facebook posts
 word_list = ["gun", "knife", "chase", "homeless", "follow", "followed", "grope", "groped", "shoot", "PSA"]
 
 @manager.command
 def index():
     for i in range(0,len(json_data)):
+        # get time string provided in Facebook json
         date_time = json_data[i]['created_time'].split("T")
+        # turn time string into datetime
         p_time = datetime.strptime(date_time[0] + date_time[1][:-5], "%Y-%m-%d%H:%M:%S")
+        # get Facebook post text
         fb_text = json_data[i]['message']
+        # check if the time of post + 1 hour is less than the current time
         if datetime.now().utcnow() <= p_time + timedelta(hours = 1):
+            # check if any word in word_list is in the Facebook post text
             for word in word_list:
                 if word in fb_text:
+                    # if one of the words is in the post, then send an email
                     mandrill_client = mandrill.Mandrill(os.environ['MANDRILL_KEY'])
                     message = {'text': fb_text,
                         'from_email': os.environ['FROM_EMAIL'],
